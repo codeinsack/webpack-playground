8b25acad5f2fbb7875d4f1b6165a50f07a5a4f5e
codeinsack <codeinsack@gmail.com>
2022-03-01 01:41:09 +0300

    split up into dev and prod configs

--- src/index.js
+++ src/index.js
@@ -5,3 +5,9 @@ const heading = new Heading();
 const button = new Button();
 heading.render();
 button.render();
+
+if (process.env.NODE_ENV === "production") {
+  console.log("Production mode");
+} else if (process.env.NODE_ENV === "development") {
+  console.log("Development mode");
+}
