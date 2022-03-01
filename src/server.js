2b1049c89174ff13433b9282cd872674552db20f
codeinsack <codeinsack@gmail.com>
2022-03-01 20:26:56 +0300

    return single html from the server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
+const path = require("path");
+const fs = require("fs");
 const express = require("express");
 const app = express();
 
+app.use("/static", express.static(path.resolve(__dirname, "../dist")));
+
 app.get("/", (req, res) => {
-  res.send("Some dummy data...");
+  const pathToHtmlFile = path.resolve(__dirname, "../dist/index.html");
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, "utf-8");
+  res.send(contentFromHtmlFile);
 });
 
 app.listen(3000, () => {
