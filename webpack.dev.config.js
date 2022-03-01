9e8745b99580617c5093094de63ba8506ff59b4b
codeinsack <codeinsack@gmail.com>
2022-03-01 16:29:56 +0300

    set up dev configuration for multiple pages

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,9 +3,12 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/hello-world.js",
+  entry: {
+    "hello-world": "./src/hello-world.js",
+    girl: "./src/girl.js",
+  },
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "./dist"),
     publicPath: "",
   },
@@ -63,9 +66,18 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin({}),
     new HtmlWebpackPlugin({
+      filename: "hello-world.html",
+      chunks: ["hello-world"],
       title: "Hello World",
       template: "src/page-template.hbs",
-      description: "Some description is here...",
+      description: "Hello World",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "girl.html",
+      chunks: ["girl"],
+      title: "Girl",
+      template: "src/page-template.hbs",
+      description: "Girl",
     }),
   ],
 };
