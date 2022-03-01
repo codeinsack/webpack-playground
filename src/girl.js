2a37517a50699e9cb9a09ddbf4e84b81d6ff0446
codeinsack <codeinsack@gmail.com>
2022-03-01 15:53:24 +0300

    splint into two bundle files

---
+++ src/girl.js
@@ -0,0 +1,8 @@
+import Heading from "./components/heading/heading";
+import Image from "./components/image/image";
+
+const heading = new Heading();
+const image = new Image();
+
+heading.render();
+image.render();
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,7 +3,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/hello-world.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "./dist"),
